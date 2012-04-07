fbdcd473a81a1f57c529e545b8f5c1d4f8a720c2
Jungho Kim <jungho@abstractfutures.com>
2012-04-07 16:09:20 -0700

    covers scroll via jquery.scrollTo + jquery.serialScroll

--- ttlg/build/js/global.js
+++ ttlg/build/js/global.js
@@ -1,11 +1,46 @@
 $(document).ready(function() {
 
 	var coverHeight = $(window).height();
+	var coverWidth = $(window).width();
+	var articleCount = $('.article-container').length;
 
+	// alert (articleCount);
 	// alert (coverHeight);
 
-	$('#cover').css({'height':((coverHeight))+'px'});
-	$('#masthead').css({'height':((coverHeight))+'px'});
+	$('.cover').css({'height':((coverHeight))+'px'});
+	$('#content-scroller-wrapper, .article-container').css({width: ((coverWidth))+'px'});
+	$('.masthead').css({'height':((coverHeight))+'px'});
+	$('#content-scroller').css({'width':((coverWidth)*(articleCount))+'px'});
+
+	/* Scrolling */
+
+	$('#content-scroller-wrapper').serialScroll({
+		items:'.article-container',
+		prev:'a.pagination-previous',
+		next:'a.pagination-next',
+		duration:1200,
+		force:true,
+		stop:true,
+		lock:false,
+		cycle:true, //don't pull back once you reach the end
+		// easing:'easeOutQuart', //use this easing equation for a funny effect
+		jump: true //click on the images to scroll to them
+	});
+
+	$('#slideshow').serialScroll({
+		items:'li',
+		prev:'#screen2 a.prev',
+		next:'#screen2 a.next',
+		offset:-230, //when scrolling to photo, stop 230 before reaching it (from the left)
+		start:1, //as we are centering it, start at the 2nd
+		duration:1200,
+		force:true,
+		stop:true,
+		lock:false,
+		cycle:false, //don't pull back once you reach the end
+		jump: true //click on the images to scroll to them
+	});
+	
 
 	{
 
@@ -42,7 +77,7 @@ $(window).resize(function() {
 
 	var $coverHeight = $(window).height();
 
-	$('#cover').css({'height':((coverHeight))+'px'});
-	$('#masthead').css({'height':((coverHeight))+'px'});
+	$('.cover').css({'height':((coverHeight))+'px'});
+	$('.masthead').css({'height':((coverHeight))+'px'});
 
 });
