af7432d33f488ae15c337426a78d6c274eb1ed15
Jungho Kim <jungho@abstractfutures.com>
2012-04-08 00:21:51 -0700

    first wowsan/interlude content, not designed - Seoul Got Soul

--- ttlg/build/js/global.js
+++ ttlg/build/js/global.js
@@ -34,6 +34,25 @@ $(document).ready(function() {
 	});
 
 	{
+		$(window).resize(function() {
+
+			var coverHeight = $(window).height();
+			var coverWidth = $(window).width();
+			var articleCount = $('.article').length;
+
+			// Set the height of the cover equal to the height of viewport
+			$('.cover').css({'height':((coverHeight))+'px'});
+			// Set the width of the container that holds the articles and the articles themselves equal to the width of the viewport
+			$('#content-scroller-wrapper, .article').css({width: ((coverWidth))+'px'});
+			// Set the height of the masthead equal to the height of the viewport so items can be locked to the top and bottom
+			$('.masthead').css({'height':((coverHeight))+'px'});
+			// Set the width of the scrollable area which contains articles equal to the width of the viewport multiplied by the number of articles
+			$('#content-scroller').css({'width':((coverWidth)*(articleCount))+'px'});
+
+		});
+	}
+
+	{
 
 		var topOffset = $('body').offset().top;
 
@@ -61,14 +80,5 @@ $(document).ready(function() {
 		});
 
 	}
-
-});
-
-$(window).resize(function() {
-
-	var $coverHeight = $(window).height();
-
-	$('.cover').css({'height':((coverHeight))+'px'});
-	$('.masthead').css({'height':((coverHeight))+'px'});
 
 });
