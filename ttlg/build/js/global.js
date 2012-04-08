8216a112127b4c98d48e0888b392a0261e98ba40
Jungho Kim <jungho@abstractfutures.com>
2012-04-07 19:08:53 -0700

    some initial design for a second article, re-factored SASS/CSS files
    seoulist.css / global css
    article_xxx.css / css for individual articles

--- ttlg/build/js/global.js
+++ ttlg/build/js/global.js
@@ -1,21 +1,27 @@
 $(document).ready(function() {
 
+	/* Set appropriate widths and heights */
+
 	var coverHeight = $(window).height();
 	var coverWidth = $(window).width();
-	var articleCount = $('.article-container').length;
+	var articleCount = $('.article').length;
 
 	// alert (articleCount);
 	// alert (coverHeight);
 
+	// Set the height of the cover equal to the height of viewport
 	$('.cover').css({'height':((coverHeight))+'px'});
-	$('#content-scroller-wrapper, .article-container').css({width: ((coverWidth))+'px'});
+	// Set the width of the container that holds the articles and the articles themselves equal to the width of the viewport
+	$('#content-scroller-wrapper, .article').css({width: ((coverWidth))+'px'});
+	// Set the height of the masthead equal to the height of the viewport so items can be locked to the top and bottom
 	$('.masthead').css({'height':((coverHeight))+'px'});
+	// Set the width of the scrollable area which contains articles equal to the width of the viewport multiplied by the number of articles
 	$('#content-scroller').css({'width':((coverWidth)*(articleCount))+'px'});
 
 	/* Scrolling */
 
 	$('#content-scroller-wrapper').serialScroll({
-		items:'.article-container',
+		items:'.article',
 		prev:'a.pagination-previous',
 		next:'a.pagination-next',
 		duration:1200,
@@ -27,34 +33,19 @@ $(document).ready(function() {
 		jump: true //click on the images to scroll to them
 	});
 
-	$('#slideshow').serialScroll({
-		items:'li',
-		prev:'#screen2 a.prev',
-		next:'#screen2 a.next',
-		offset:-230, //when scrolling to photo, stop 230 before reaching it (from the left)
-		start:1, //as we are centering it, start at the 2nd
-		duration:1200,
-		force:true,
-		stop:true,
-		lock:false,
-		cycle:false, //don't pull back once you reach the end
-		jump: true //click on the images to scroll to them
-	});
-	
-
 	{
 
 		var topOffset = $('body').offset().top;
 
 		//alert ($('.navigation').offset());
 
-		$(window).scroll(function(e) {
+		$(window).scroll(function() {
 			// Detect if content is being scroll offscreen.
 
 			if ( (document.documentElement.scrollTop || document.body.scrollTop) >= ((coverHeight)/2))
 			{
 				$('.pagination-previous, .pagination-next').css('opacity', 0);
-				/* Commented out because it makes scrolling really, really slow
+				/* Commented out because it needs some kind of thing to stop it from animating a ton
 				$('.pagination-previous, .pagination-next').animate({
 					opacity: 0
 				}, 500);
