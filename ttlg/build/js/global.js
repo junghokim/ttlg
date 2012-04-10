d6c82825bbcecb7ffcae3085e8189440204ff301
Jungho Kim <jungho@abstractfutures.com>
2012-04-10 09:53:30 -0700

    split the home screen vertically into image area and content area with seoulist selects description

--- ttlg/build/js/global.js
+++ ttlg/build/js/global.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
 	// alert (coverHeight);
 
 	// Set the height of the cover equal to the height of viewport
-	$('.cover').css({'height':((coverHeight))+'px'});
+	$('.cover').css({'height':((coverHeight)*.7)+'px'});
 	// Set the width of the container that holds the articles and the articles themselves equal to the width of the viewport
 	$('#content-scroller-wrapper, .article').css({width: ((coverWidth))+'px'});
 	// Set the height of the masthead equal to the height of the viewport so items can be locked to the top and bottom
@@ -44,7 +44,8 @@ $(document).ready(function() {
 			var articleCount = $('.article').length;
 
 			// Set the height of the cover equal to the height of viewport
-			$('.cover').css({'height':((coverHeight))+'px'});
+			// Changed to accomodate things below cover on homepage
+			$('.cover').css({'height':((coverHeight)*.7)+'px'});
 			// Set the width of the container that holds the articles and the articles themselves equal to the width of the viewport
 			$('#content-scroller-wrapper, .article').css({width: ((coverWidth))+'px'});
 			// Set the height of the masthead equal to the height of the viewport so items can be locked to the top and bottom
