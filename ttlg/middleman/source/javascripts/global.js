73710482816dc21e547541ba0d301bdf87f0392d
Jungho Kim <jungho@abstractfutures.com>
2012-04-16 01:17:53 -0700

    fixed a bunch of small bugs with scrolling, but not hashtags. designed cover and added text of an actor's map of seoul.

--- ttlg/middleman/source/javascripts/global.js
+++ ttlg/middleman/source/javascripts/global.js
@@ -6,12 +6,46 @@ $(document).ready(function() {
 	var coverWidth = $(window).width();
 	var articleCount = $('.article').length;
 
+	$('#vertical').each(function() {
+			var $children = $(this).children(),
+					count = $children.size(),
+					$item;
+			$children.each(function(i) {
+					$item = $(this)
+							.css({'left':((i + 1)*10)+'%'});
+					if (i === 0) {
+							$item.addClass('first');
+					}
+					if (i == count - 1) {
+							$item.addClass('last');
+					}
+			});
+	});
+
+	$('#horizontal').each(function() {
+			var $children = $(this).children(),
+					count = $children.size(),
+					$item;
+			$children.each(function(i) {
+					var coverHeight = $(window).height();
+					$item = $(this)
+							.css({'top':(((coverHeight)-62)*(i + 1))*.1});
+					if (i === 0) {
+							$item.addClass('first');
+					}
+					if (i == count - 1) {
+							$item.addClass('last');
+					}
+			});
+	});	
+
+
 	// alert (articleCount);
 	// alert (coverHeight);
 
 	// Set the height of the cover equal to the height of viewport
 	// Subtract the height of the #masthead (3.9375em x 16px = 63px)
-	$('.cover').css({'height':((coverHeight)-61)+'px'});
+	$('.cover').css({'height':((coverHeight)-62)+'px'});
 	// Set height of covers on homepage
 	$('#home .article').css({'height':(((coverHeight)*.7)-62)+'px'});
 	// Set the width of the container that holds the articles and the articles themselves equal to the width of the viewport
@@ -40,6 +74,8 @@ $(document).ready(function() {
 		// easing:'easeOutQuart', //use this easing equation for a funny effect
 		jump: true //click on the images to scroll to them
 	});
+
+
 
 	// Local Scroll
 
@@ -82,6 +118,40 @@ $(document).ready(function() {
 			$('.slideshow-image').css({'width':((coverWidth))+'px'});
 			//$('#article-list').css({'height':((coverHeight))+'px'});
 
+			$('#vertical').each(function() {
+					var $children = $(this).children(),
+							count = $children.size(),
+							$item;
+					$children.each(function(i) {
+							$item = $(this)
+									.css({'left':((i + 1)*10)+'%'});
+							if (i === 0) {
+									$item.addClass('first');
+							}
+							if (i == count - 1) {
+									$item.addClass('last');
+							}
+					});
+			});
+
+			$('#horizontal').each(function() {
+					var $children = $(this).children(),
+							count = $children.size(),
+							$item;
+					$children.each(function(i) {
+							var coverHeight = $(window).height();
+							$item = $(this)
+									.css({'top':(((coverHeight)-62)*(i + 1))*.1});
+							if (i === 0) {
+									$item.addClass('first');
+							}
+							if (i == count - 1) {
+									$item.addClass('last');
+							}
+					});
+			});	
+			
+
 		});
 	}
 
@@ -96,64 +166,94 @@ $(document).ready(function() {
 
 			if ( (document.documentElement.scrollTop || document.body.scrollTop) >= ((coverHeight)/2))
 			{
-				//$('.pagination-previous, .pagination-next').css('opacity', 0);
-				/* Commented out because it needs some kind of thing to stop it from animating a ton
-				$('.pagination-previous, .pagination-next').animate({
-					opacity: 0
-				}, 500);
-				*/
 			}
 			else
 				$('.pagination-previous, .pagination-next').css('opacity', 0.7);
-				/*
-				$('.pagination-previous, .pagination-next').animate({
-					opacity: 0.7
-				}, 500);
-				*/
 		});
 
 	}
 
-  $("#home > .content-scroller-wrapper").slides({
-    preload: false,
-    play: 5000,
-    bigTarget: false,
+	$("#home > .content-scroller-wrapper").slides({
+		preload: false,
+		play: 5000,
+		bigTarget: false,
 		effect: 'slide',
 		crossfade: true,
 		slideSpeed: 500,
 		pagination: true,
 		generateNextPrev: false,
 		generatePagination: true
-  });
+	});
 
-  /* Movement Tests */
+	/* Movement Tests */
+
+/*
+	$('#home .content-scroller a').click(function() {
+		$('body').removeClass('home').addClass('articles');
+		$('#home').slideUp('slow').fadeOut('slow',
+			function() {
+				$('#articles').fadeIn('slow');
+				// Removing #home from DOM lets localscroll work by not creating an ID conflict with articles (previously they were both in the featured area in #home and within #articles as themselves)
+				$('#home').remove();
+		});
+	});
+*/
 
 	$('#home .content-scroller a').click(function() {
-	  $('body').removeClass('home').addClass('articles');
-	  $('#home').slideUp('slow').fadeOut('slow',
-	  	function() {
-    		$('#articles').fadeIn('slow');
-    		// Removing #home from DOM lets localscroll work by not creating an ID conflict with articles (previously they were both in the featured area in #home and within #articles as themselves)
-    		$('#home').remove();
-  	});
+		$('body').removeClass('home').addClass('articles');
+		$('#home').slideUp('slow').fadeOut('slow',
+			function() {
+				$('#articles').load('articles.html',
+					function() {
+
+						// Set the height of the cover equal to the height of viewport
+						// Changed to accomodate things below cover on homepage
+						$('.cover').css({'height':((coverHeight)-62)+'px'});
+					
+						// Set the width of the container that holds the articles and the articles themselves equal to the width of the viewport
+						$('.content-scroller-wrapper, .article').css({width: ((coverWidth))+'px'});
+
+						// Set the width of the scrollable area which contains articles equal to the width of the viewport multiplied by the number of articles
+						$('#content-scroller').css({'width':((coverWidth)*(articleCount))+'px'});
+
+						// Having to call this again for some reason after load();
+
+						$('#articles .content-scroller-wrapper').serialScroll({
+							items:'.article',
+							prev:'a.pagination-previous',
+							next:'a.pagination-next',
+							duration:1200,
+							force:true,
+							stop:true,
+							lock:false,
+							cycle:true, //don't pull back once you reach the end
+							// easing:'easeOutQuart', //use this easing equation for a funny effect
+							jump: true //click on the images to scroll to them
+						});
+
+					});
+				// Removing #home from DOM lets localscroll work by not creating an ID conflict with articles (previously they were both in the featured area in #home and within #articles as themselves)
+				$('#home').remove();
+		});
 	});
 
-	$('#events a').click(function() {
-	  $('body').removeClass('home').addClass('articles');
-	  $('#home').slideUp('slow').fadeOut('slow',
-	  	function() {
-    		$('#articles').fadeIn('slow');
-    		// Removing #home from DOM lets localscroll work by not creating an ID conflict with articles (previously they were both in the featured area in #home and within #articles as themselves)
-    		$('#home').remove();
-  	});
-  	// Not working !!!
-  	$(this).localScroll({
-		target: '.content-scroller-wrapper'
-	}); 
+	$('#events a').click(function() { 	
+		$('body').removeClass('home').addClass('articles');
+		$('#home').slideUp('slow').fadeOut('slow',
+			function() {
+				$('#articles').load('articles.html');
+
+				// Removing #home from DOM lets localscroll work by not creating an ID conflict with articles (previously they were both in the featured area in #home and within #articles as themselves)
+				$('#home').remove();
+		});
+		// Not working !!!
+		$(this).localScroll({
+			target: '.content-scroller-wrapper'
+		}); 
 	});
 
 
-  /* Switch */
+	/* Switch */
 
 	/*$('#trigger.closed').click ( function () {
 		$('#trigger').removeClass('closed').addClass('open');
@@ -161,9 +261,11 @@ $(document).ready(function() {
 	});*/
 
 	$('#trigger, #close').click(function() {
-	  $('#table-of-contents').slideToggle('slow', function() {
-	    $('#curtain').hide();
-	  });
+		$('#table-of-contents').slideToggle('slow', function() {
+			$('#curtain').hide();
+		});
 	});
+
+
 
 });
